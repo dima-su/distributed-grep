a913a14bc003dc1866a6aba89dd12014ab913ce1
Dima Su <arezxbusiness@gmail.com>
2026-04-23 01:48:55 +0300

    Added connectedWorkers in client struct

--- logic/master/engine.go
+++ logic/master/engine.go
@@ -1,10 +1,67 @@
 package master
 
 import (
-	_ "grep-distributed/proto-generated/proto-generated"
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	gen "grep-distributed/api"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-type GrepServer struct{}
+type GrepClient struct {
+	workerAdresses []string
+	connectedWorkers []grpc.ClientConn
+}
 
-func main() {
+func (client *GrepClient) ConnectWorkers (error){
+		for _, address := range client.workerAdresses {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Printf("grpc couldn't establish connection with %v. error code: %v", address, err)
+			return []string{}, err
+		} else {
+			client.connectedWorkers = append(client.connectedWorkers, conn)
+			log.Printf("gRPC established connection with worker on address %v", address)
+		}
+	
+}
+
+func (client *GrepClient) CallAllWorkers(query string) ([]string, error) {
+	resultChan := make(chan *gen.GrepResponse, len(client.workerAdresses))
+	var wg sync.WaitGroup
+
+	log.Println("Calling all workers. . .")
+
+	for _, conn := range client.connectedWorkers {
+		wg.Add(1)
+		reciever := gen.NewDistributedGrepClient(conn)
+
+		go func(worker gen.DistributedGrepClient) {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			oneAnsw, err := reciever.Grep(ctx, &gen.GrepRequest{Query: query})
+			if err == nil {
+				resultChan <- oneAnsw
+			} else {
+				log.Printf("Worker %v couldn't grep. Error code: %v", address, err)
+			}
+		}(reciever)
+	}
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+	log.Println("Got results from workers, summing up. . .")
+	var result []string
+	for res := range resultChan {
+		result = append(result, res.Matches...)
+	}
+	log.Println("Called all workers succesfully")
+
+	return result, nil
 }
